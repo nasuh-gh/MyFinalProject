5aa6a6c8910df04fef94e2522cefe75acd6db059
nasuh-gh <nasuhguler@gmail.com>
2021-02-12 02:44:45 +0300

    6 Şubat Ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,14 +11,23 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //Data Transformation Object-taşınacak objeler-join gibi
+
             //ProductTest();
 
+            //CategoryTest();
+
+            ProductTest2();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
@@ -30,5 +39,16 @@ namespace ConsoleUI
                 Console.WriteLine(product.ProductName);
             }
         }
+
+        private static void ProductTest2()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName+" / "+product.CategoryName);
+            }
+        }
+
     }
 }
