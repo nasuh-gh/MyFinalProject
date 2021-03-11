11a0a42e915b200be32d4d65054950507829c3ef
nasuh-gh <nasuhguler@gmail.com>
2021-03-12 01:58:36 +0300

    12

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -13,9 +13,9 @@ namespace Business.ValidationRules.FluentValidation
             //Product ile ilgili kurallar
             RuleFor(p => p.ProductName).NotEmpty();//boş olamaz
             RuleFor(p => p.ProductName).MinimumLength(2);
-            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).NotEmpty();//boş olamaz
             RuleFor(p => p.UnitPrice).GreaterThan(0);//0 dan büyük olmalı
-            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);//Kategori id si 1 olan ürüün fiyatı 10 dan büyük olmalı
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);//Kategori id si 1 olan ürün fiyatı 10 dan büyük olmalı
             RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
 
         }
